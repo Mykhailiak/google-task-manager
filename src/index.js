8d5751be26cbf290b0be1eaa0e27800fc26acbcf
Yura Myhaylyak <ghost86421@gmail.com>
2017-07-02 16:53:23 +0300

    Inject redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './styles/index.css';
 import App from './components/App';
-import './api/index';
+import createStore from './store/createStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
