ab9b507fcf90158eef146c9dc572ecbf4e556276
Yura Myhaylyak <ghost86421@gmail.com>
2017-07-06 22:38:07 +0300

    Added loading field to external-loaded data

--- src/reducers/tasksListItem.js
+++ src/reducers/tasksListItem.js
@@ -1,24 +1,33 @@
 import {
   TASKS_LIST_ITEM_SUCCESS,
-  TASKS_LIST_ITEM_FAIL
+  TASKS_LIST_ITEM_FAIL,
+  TASKS_LIST_ITEM_REQUEST
 } from '../constants/tasksListItem';
 
 const initialData = {
   error: false,
+  loading: false,
   list: [],
 };
 
 export default function tasksListItemReducer(state = initialData, action) {
   switch (action.type) {
+  case TASKS_LIST_ITEM_REQUEST:
+    return {
+      ...state,
+      loading: true,
+    };
   case TASKS_LIST_ITEM_SUCCESS:
     return {
       ...state,
       error: false,
+      loading: false,
       list: action.list,
     };
   case TASKS_LIST_ITEM_FAIL:
     return {
       ...state,
+      loading: false,
       error: true,
     };
   default:
--- src/reducers/tasksLists.js
+++ src/reducers/tasksLists.js
@@ -1,20 +1,28 @@
 import {
   TASKS_LISTS_SUCCESS,
-  TASKS_LISTS_FAIL
+  TASKS_LISTS_FAIL,
+  TASKS_LISTS_REQUEST
 } from '../constants/tasksLists';
 
 const initialState = {
   error: false,
   errorMessage : '',
+  loading: false,
   list: [],
 };
 
 export default function tasksListReducer(state = initialState, action) {
   switch(action.type) {
+  case TASKS_LISTS_REQUEST:
+    return {
+      ...state,
+      loading: true,
+    };
   case TASKS_LISTS_FAIL:
     return {
       ...state,
       error: false,
+      loading: false,
       errorMessage: action.error,
     };
   case TASKS_LISTS_SUCCESS: {
@@ -22,6 +30,7 @@ export default function tasksListReducer(state = initialState, action) {
       ...state,
       error: false,
       errorMessage: '',
+      loading: false,
       list: action.payload,
     };
   }
